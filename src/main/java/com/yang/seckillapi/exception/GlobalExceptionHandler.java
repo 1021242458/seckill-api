570ab6a3115739dbe78187d0c69093e49a8b5de5
xxx <1021242458@qq.com>
2024-12-01 23:06:08 +0800

    测试流水线

--- src/main/java/com/yang/seckillapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yang/seckillapi/exception/GlobalExceptionHandler.java
@@ -1,15 +1,16 @@
 package com.yang.seckillapi.exception;
 
-import com.yang.seckillapi.pojo.Result;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.yang.seckillapi.pojo.Result;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public Result handleException(Exception e) {
         e.printStackTrace();
-        return Result.error(StringUtils.hasLength(e.getMessage()) ? e.getMessage() : " 操作失败--流水线测试");
+        return Result.error(StringUtils.hasLength(e.getMessage()) ? e.getMessage() : " 操作失败");
     }
 }
